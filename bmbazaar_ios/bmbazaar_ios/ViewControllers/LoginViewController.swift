052684eecb5fada8ccfb08fd65f2ae508f393310
Nitisha Bhandari <nitisha.bhandari0@gmail.com>
2022-02-06 18:25:46 -0500

    Set up login page

---
+++ bmbazaar_ios/bmbazaar_ios/ViewControllers/LoginViewController.swift
@@ -0,0 +1,22 @@
+//
+//  LoginViewController.swift
+//  bmbazaar_ios
+//
+//  Created by Nitisha on 2/5/22.
+//
+
+import Foundation
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var emailText: UITextField!
+    @IBOutlet weak var passwordText: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+}
