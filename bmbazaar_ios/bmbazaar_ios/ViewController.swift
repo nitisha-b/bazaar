a7971739928165f7a0fcd0b9e59e9d7b1f8b7834
Nitisha Bhandari <nitisha.bhandari0@gmail.com>
2022-02-05 19:15:05 -0500

    Add xcode project

---
+++ bmbazaar_ios/bmbazaar_ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  bmbazaar_ios
+//
+//  Created by Nitisha on 2/5/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
