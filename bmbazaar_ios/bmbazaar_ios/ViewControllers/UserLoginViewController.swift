b148d4dcb7ccd206f47bbfa79ebe69f1d8805cce
Nitisha Bhandari <nitisha.bhandari0@gmail.com>
2022-02-07 23:22:59 -0500

    UI for guest login

---
+++ bmbazaar_ios/bmbazaar_ios/ViewControllers/UserLoginViewController.swift
@@ -0,0 +1,22 @@
+//
+//  UserLoginViewController.swift
+//  bmbazaar_ios
+//
+//  Created by Nitisha on 2/7/22.
+//
+
+import Foundation
+import UIKit
+
+class UserLoginViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+//        guestLoginButton.adjustsImageSizeForAccessibilityContentSizeCategory = true;
+        
+      
+    }
+    
+}
