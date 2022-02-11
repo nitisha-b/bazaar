146ab8ac080593ba62e406060a9db497dc9b8ca4
Nitisha Bhandari <nitisha.bhandari0@gmail.com>
2022-02-10 23:23:11 -0500

    make the web folder

---
+++ bmbazaar_web/Item.js
@@ -0,0 +1,24 @@
+var mongoose = require('mongoose');
+
+// the host:port must match the location where you are running MongoDB
+// the "myDatabase" part can be anything you like
+mongoose.connect("mongodb+srv://ndaniyarov:HelloNitisha@cluster0.sazvw.mongodb.net/items_services?retryWrites=true&w=majority");
+
+var Schema = mongoose.Schema;
+
+var itemSchema = new Schema({
+ title: {type: String, required: true},
+ description: String,
+ isService: Boolean,
+ venmo: String,
+ location: String,
+ price: {type: Number, required: true}
+});
+
+// export personSchema as a class called Person
+module.exports = mongoose.model('Item', itemSchema);
+
+itemSchema.methods.standardizeName = function() {
+    this.title = this.title.toLowerCase();
+    return this.title;
+}
